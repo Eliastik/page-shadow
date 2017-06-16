12fe98525f87e91f5635d401205ed63eba41b7e0
Eliastik <eliastik@eliastiksofts.com>
2017-06-16 18:03:23 +0200

    Improved gulp file and moved manifests files

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,39 @@ gulp.task('clean', function() {
 		.pipe(clean());
 });
 
-gulp.task('build', function() {
-	var manifest = require('./src/manifest'),
-		distFileName = manifest.name + ' v' + manifest.version;
-    var codebase = manifest.codebase;
-	gulp.src('build/*', {read: false})
-		.pipe(clean());
-    gulp.src('./src/**')
-        .pipe(zip(distFileName + '.zip'))
-        .pipe(gulp.dest('./build'));
-    gulp.src("./src/**")
+gulp.task('copy', function() {
+	gulp.src(['./src/**', './manifests/chrome/manifest.json'])
+        .pipe(gulp.dest('./build/chrome/'));
+    gulp.src(['./src/**', './manifests/edge/**'])
+        .pipe(gulp.dest('./build/edge/'));
+    return gulp.src(['./src/**', './manifests/firefox/manifest.json'])
+        .pipe(gulp.dest('./build/firefox/'));
+});
+
+gulp.task('build', ['copy'], function() {
+	var manifestChrome = require('./manifests/chrome/manifest'),
+	    manifestEdge = require('./manifests/edge/manifest'),
+	    manifestFirefox = require('./manifests/firefox/manifest'),
+		distFileName = manifestChrome.name + ' v' + manifestChrome.version;
+    var codebase = manifestChrome.codebase;
+    gulp.src("./build/firefox/**")
         .pipe(zip(distFileName + '.xpi'))
         .pipe(gulp.dest('./build'));
-    return gulp.src('./src/')
+    gulp.src("./build/edge/**")
+        .pipe(zip(distFileName + ' Edge' +'.zip'))
+        .pipe(gulp.dest('./build'));
+    gulp.src("./build/chrome/**")
+        .pipe(zip(distFileName + ' Opera' +'.zip'))
+        .pipe(gulp.dest('./build'));
+    return gulp.src('./build/chrome')
         .pipe(crx({
           privateKey: fs.readFileSync('./key/key.pem', 'utf8'),
-          filename: manifest.name + ' v' + manifest.version + '.crx',
+          filename: manifestChrome.name + ' v' + manifestChrome.version + '.crx',
           codebase: codebase,
         }))
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['clean'], function() {
+    gulp.start('build');
+});
