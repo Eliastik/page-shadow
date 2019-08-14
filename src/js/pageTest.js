6866973ed4ddd9694680ae8dfcb15ec4ab06ebb1
Eliastik <26941242+Eliastik@users.noreply.github.com>
2019-08-14 03:19:17 +0200

    Updated libraries + fixes

--- src/js/pageTest.js
+++ src/js/pageTest.js
@@ -1,19 +1,19 @@
 /* Page Shadow
- * 
- * Copyright (C) 2015-2018 Eliastik (eliastiksofts.com)
- * 
+ *
+ * Copyright (C) 2015-2019 Eliastik (eliastiksofts.com)
+ *
  * This file is part of Page Shadow.
- * 
+ *
  * Page Shadow is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Page Shadow is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Page Shadow.  If not, see <http://www.gnu.org/licenses/>. */
 /* translation */
