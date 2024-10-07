0d48ed5ff46aea14f9a0c8867cb30ba222beb3b6
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-10-07 22:29:08 +0200

    Refactor Shadow DOM code to another class

--- src/js/classes/pageAnalyzer.js
+++ src/js/classes/pageAnalyzer.js
@@ -16,10 +16,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Page Shadow.  If not, see <http://www.gnu.org/licenses/>. */
-import { getCustomThemeConfig, processRules, removeClass, addClass, processRulesInvert, loadWebsiteSpecialFiltersConfig, rgb2hsl } from "../utils/util.js";
-import { ignoredElementsContentScript, defaultThemesBackgrounds, defaultThemesLinkColors, defaultThemesVisitedLinkColors, defaultThemesTextColors, pageShadowClassListsMutationsIgnore, ignoredElementsBrightTextColorDetection } from "../constants.js";
+import { removeClass, addClass, loadWebsiteSpecialFiltersConfig, rgb2hsl } from "../utils/util.js";
+import { ignoredElementsContentScript, pageShadowClassListsMutationsIgnore, ignoredElementsBrightTextColorDetection } from "../constants.js";
 import ThrottledTask from "./throttledTask.js";
 import ImageProcessor from "./imageProcessor.js";
+import ShadowDomProcessor from "./shadowDomProcessor.js";
 
 /**
  * Class used to analyze the pages and detect transparent background,
@@ -28,6 +29,7 @@ import ImageProcessor from "./imageProcessor.js";
 export default class PageAnalyzer {
 
     imageProcessor;
+    shadowDomProcessor;
 
     websiteSpecialFiltersConfig = {};
     isEnabled = false;
@@ -53,6 +55,7 @@ export default class PageAnalyzer {
         this.multipleElementClassBatcherRemove = multipleElementClassBatcherRemove;
         this.debugLogger = debugLogger;
         this.imageProcessor = new ImageProcessor(this.debugLogger, websiteSpecialFiltersConfig);
+        this.shadowDomProcessor = new ShadowDomProcessor(currentSettings, isEnabled);
 
         this.initializeThrottledTasks();
     }
@@ -68,6 +71,11 @@ export default class PageAnalyzer {
 
         if(this.imageProcessor) {
             this.imageProcessor.websiteSpecialFiltersConfig = websiteSpecialFiltersConfig;
+        }
+
+        if(this.shadowDomProcessor) {
+            this.shadowDomProcessor.currentSettings = currentSettings;
+            this.shadowDomProcessor.isEnabled = isEnabled;
         }
     }
 
@@ -322,6 +330,14 @@ export default class PageAnalyzer {
         }
     }
 
+    processShadowRoot(currentElement) {
+        this.shadowDomProcessor.processShadowRoot(currentElement);
+    }
+
+    resetShadowRoots() {
+        this.shadowDomProcessor.resetShadowRoots();
+    }
+
     isTextElement(element) {
         if (!element || element.nodeType !== Node.ELEMENT_NODE || ignoredElementsBrightTextColorDetection.includes(element.tagName.toLowerCase())) {
             return false;
@@ -485,93 +501,5 @@ export default class PageAnalyzer {
                 }
             }
         }
-    }
-
-    processShadowRoot(currentElement) {
-        if(currentElement) {
-            if(currentElement.shadowRoot != null) {
-                this.processOneShadowRoot(currentElement);
-                const elementChildrens = currentElement.shadowRoot.querySelectorAll("*");
-
-                if(elementChildrens && elementChildrens.length > 0) {
-                    for(let i = 0, len = elementChildrens.length; i < len; i++) {
-                        this.processShadowRoot(elementChildrens[i]);
-                    }
-                }
-            }
-        }
-    }
-
-    async processOneShadowRoot(element) {
-        if(element.shadowRoot) {
-            const currentCSSStyle = element.shadowRoot.querySelector(".pageShadowCSSShadowRoot");
-            const currentCSSStyleInvert = element.shadowRoot.querySelector(".pageShadowCSSShadowRootInvert");
-
-            if(currentCSSStyle) {
-                element.shadowRoot.removeChild(currentCSSStyle);
-            }
-
-            if(currentCSSStyleInvert) {
-                element.shadowRoot.removeChild(currentCSSStyleInvert);
-            }
-
-            if(this.isEnabled && ((this.currentSettings.pageShadowEnabled != undefined && this.currentSettings.pageShadowEnabled == "true") || (this.currentSettings.colorInvert != undefined && this.currentSettings.colorInvert == "true") || (this.currentSettings.attenuateColors != undefined && this.currentSettings.attenuateColors == "true"))) {
-                if(this.currentSettings.pageShadowEnabled != undefined && this.currentSettings.pageShadowEnabled == "true") {
-                    const currentTheme = this.currentSettings.theme;
-
-                    if(currentTheme != null) {
-                        const styleTag = document.createElement("style");
-                        styleTag.classList.add("pageShadowCSSShadowRoot");
-                        element.shadowRoot.appendChild(styleTag);
-
-                        let themeConfig = {};
-
-                        if(currentTheme.startsWith("custom")) {
-                            themeConfig = await getCustomThemeConfig(this.currentSettings.theme.replace("custom", ""));
-                        } else {
-                            themeConfig = {
-                                backgroundColor: defaultThemesBackgrounds[currentTheme - 1].replace("#", ""),
-                                textColor: defaultThemesLinkColors[currentTheme - 1].replace("#", ""),
-                                linkColor: defaultThemesVisitedLinkColors[currentTheme - 1].replace("#", ""),
-                                visitedLinkColor: defaultThemesTextColors[currentTheme - 1].replace("#", "")
-                            };
-                        }
-
-                        processRules(styleTag, themeConfig, true);
-                    }
-                }
-
-                if(this.currentSettings.colorInvert != undefined && this.currentSettings.colorInvert == "true") {
-                    const styleTagInvert = document.createElement("style");
-                    styleTagInvert.classList.add("pageShadowCSSShadowRootInvert");
-                    element.shadowRoot.appendChild(styleTagInvert);
-
-                    processRulesInvert(styleTagInvert, this.currentSettings.colorInvert, this.currentSettings.invertImageColors, this.currentSettings.invertEntirePage, this.currentSettings.invertVideoColors, this.currentSettings.invertBgColor, this.currentSettings.selectiveInvert);
-                }
-
-                this.processedShadowRoots.push(element.shadowRoot);
-            }
-        }
-    }
-
-    resetShadowRoots() {
-        for(let i = 0, len = this.processedShadowRoots.length; i < len; i++) {
-            const shadowRoot = this.processedShadowRoots[i];
-
-            if(shadowRoot) {
-                const currentCSSStyle = shadowRoot.querySelector(".pageShadowCSSShadowRoot");
-                const currentCSSStyleInvert = shadowRoot.querySelector(".pageShadowCSSShadowRootInvert");
-
-                if(currentCSSStyle) {
-                    shadowRoot.removeChild(currentCSSStyle);
-                }
-
-                if(currentCSSStyleInvert) {
-                    shadowRoot.removeChild(currentCSSStyleInvert);
-                }
-            }
-        }
-
-        this.processedShadowRoots = [];
     }
 }
