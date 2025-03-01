564d0c1d019063c6bb1054f041926317b418b2ce
Eliastik <26941242+Eliastik@users.noreply.github.com>
2025-03-01 15:48:13 +0100

    Added new special filter

--- src/js/classes/pageAnalyzer.js
+++ src/js/classes/pageAnalyzer.js
@@ -117,7 +117,7 @@ export default class PageAnalyzer {
         this.debugLogger?.log("PageAnalyzer setupThrottledTasks - Setup throttled tasks", "debug");
 
         this.throttledTaskAnalyzeElements = this.throttledTaskAnalyzeElements || new ThrottledTask(
-            element => this.processElement(element, true),
+            element => this.processElement(element, !this.websiteSpecialFiltersConfig.throttleBackgroundDetectionDestylePerElement),
             "throttledTaskAnalyzeElements"
         );
 
@@ -143,8 +143,17 @@ export default class PageAnalyzer {
                 this.websiteSpecialFiltersConfig.throttleBackgroundDetectionMaxExecutionTime
             );
 
-            this.throttledTaskAnalyzeElements.callbackBeforeStart = () => addClass(document.body, "pageShadowDisableStyling");
-            this.throttledTaskAnalyzeElements.callbackAfterFinish = () => removeClass(document.body, "pageShadowDisableStyling");
+            this.throttledTaskAnalyzeElements.callbackBeforeStart = () => {
+                if(!this.websiteSpecialFiltersConfig.throttleBackgroundDetectionDestylePerElement) {
+                    addClass(document.body, "pageShadowDisableStyling");
+                }
+            };
+
+            this.throttledTaskAnalyzeElements.callbackAfterFinish = () => {
+                if(!this.websiteSpecialFiltersConfig.throttleBackgroundDetectionDestylePerElement) {
+                    removeClass(document.body, "pageShadowDisableStyling");
+                }
+            };
         }
 
         if(this.throttledTaskAnalyzeSubchilds) {
